50987cd0352d06d33d05d5b93d0391703c221238
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-12 12:43:50 -0500

    Creacion del modulo sidemenu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidemenuComponent
+  ],
   imports: [
     CommonModule
   ]
