25dfbd36b9a803ca9c8463fa17827b6ff65cb4a6
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-14 12:28:32 -0500

    Creacion de formulario basico

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+  initform= {
+    producto: 'RTX 4080',
+    precio: 0,
+    existencias: 0
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  nombreValido():boolean{
+    return this.miFormulario?.controls['producto']?.invalid &&
+            this.miFormulario?.controls['producto']?.touched ;
+  }
+
+  precioValido():boolean{ 
+    return  this.miFormulario?.controls['precio']?.touched  &&
+            this.miFormulario?.controls['precio']?.value < 0;
+  }
+
+  guardar(  ){
+    console.log(this.miFormulario);
+    this.miFormulario.resetForm({
+      precio: 0,
+      existencias:0
+    });
   }
 
 }
