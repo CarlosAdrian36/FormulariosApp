be6547919cd13ec53b7b4eaccebfaf814e4f8e42
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-19 11:44:02 -0500

    Creacion de componente registro y login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistroComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
