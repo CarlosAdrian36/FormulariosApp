607e2974ed1dd2a794af8d8d77256d6b75d9386f
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-20 10:50:05 -0500

    Creacion de validaciones y funcionalidad de registo

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, ValidationErrors } from '@angular/forms';
+import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from '../../../shared/validator/validaciones';
+import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+
+
+  miFormulario : FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.pattern(this.vs.nombreApellidoPattern) ]],
+    email: [ '', [ Validators.required, Validators.pattern(this.vs.emailPattern) ]],
+    username: [ '', [ Validators.required, this.vs.noPuedeSerStrider ] ],
+    
+  })
+
+  constructor( private fb: FormBuilder,
+                private vs : ValidatorService ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Carlos Arroyo',
+      email: 'test@test.com',
+      username: 'CRLLArroyo'
+
+    })
+  }
+
+  campoNoValido(campo:string){
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario(){
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched();
   }
 
 }
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -48,6 +48,18 @@ export class SidemenuComponent   {
       ruta: './reactive/switches'
     },
     
+  ];
+
+  authMenu: MenuItem[] = [
+
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    }
   ]
 
 }
