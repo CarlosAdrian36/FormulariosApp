cdffee6f9fa10f60b1548781648b1cd68dd10a26
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-07-21 09:54:14 -0500

    Creacion de validaciones de correo

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, ValidationErrors } from '@angular/forms';
 import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from '../../../shared/validator/validaciones';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -15,7 +16,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario : FormGroup = this.fb.group({
     nombre: [ '', [ Validators.required, Validators.pattern(this.vs.nombreApellidoPattern) ]],
-    email: [ '', [ Validators.required, Validators.pattern(this.vs.emailPattern) ]],
+    email: [ '', [ Validators.required, Validators.pattern(this.vs.emailPattern) ],[this.emailvalidator ]],
     username: [ '', [ Validators.required, this.vs.noPuedeSerStrider ] ],
     password: [ '', [ Validators.required, Validators.minLength(6) ] ],
     confirmacion: [ '', [ Validators.required, ] ],
@@ -25,15 +26,32 @@ export class RegistroComponent implements OnInit {
   })
 
   constructor( private fb: FormBuilder,
-                private vs : ValidatorService ) { }
+                private vs : ValidatorService,
+                private emailvalidator : EmailValidatorService ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Carlos Arroyo',
       email: 'test@test.com',
-      username: 'CRLLArroyo'
+      username: 'CRLLArroyo',
+      password: '123456',
+      confirmacion: '123456'
 
     })
+  }
+  emailexiste(){
+    return this.miFormulario.get('email')?.errors?.['required'] 
+    && this.miFormulario.get('email')?.touched;
+  }
+
+  emailformato(){
+    return this.miFormulario.get('email')?.errors?.['pattern'] 
+    && this.miFormulario.get('email')?.touched;
+  }
+
+  emailtomado(){
+    return this.miFormulario.get('email')?.errors?.['emailTomado'] 
+    && this.miFormulario.get('email')?.touched;
   }
 
   campoNoValido(campo:string){
